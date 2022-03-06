5dfe2447258b1a76789929a622f91f0d58880721
Fabricio Zeferino <37294999+fabriciozeferino@users.noreply.github.com>
2022-03-06 17:18:27 +0000

    frontend - add Vue 3

---
+++ webpack.mix.js
@@ -0,0 +1,11 @@
+const mix = require('laravel-mix');
+
+mix.browserSync({
+	proxy: process.env.APP_URL,
+	notify: true
+});
+
+mix.js('resources/js/app.js', 'public/js').vue();
+mix.postCss('resources/css/app.css', 'public/css', [
+	//
+]);
