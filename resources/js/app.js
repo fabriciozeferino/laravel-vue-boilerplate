3fecf1a9af703e327b94dfa7e54f19c23ff6dbcc
Fabricio Zeferino <37294999+fabriciozeferino@users.noreply.github.com>
2022-03-12 15:27:46 +0000

    set boilerplate app after authenticated

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,8 @@
-require("./bootstrap");
+require('./bootstrap')
 
-import {createApp} from "vue";
-import App from "./App.vue";
+import {createApp} from 'vue'
+import App from './App.vue'
 
-const app = createApp(App);
+const app = createApp(App)
 
-app.mount("#app");
+app.mount('#app')
