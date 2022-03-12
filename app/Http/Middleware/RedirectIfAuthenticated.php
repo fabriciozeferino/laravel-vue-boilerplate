c1cee0f2bc21886a5f2d45b2199432c9423de546
Fabricio Zeferino <37294999+fabriciozeferino@users.noreply.github.com>
2022-03-12 15:10:54 +0000

    Redirect to Home if already authenticated

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -4,7 +4,9 @@ namespace App\Http\Middleware;
 
 use App\Providers\RouteServiceProvider;
 use Closure;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Auth;
 
 class RedirectIfAuthenticated
@@ -17,14 +19,10 @@ class RedirectIfAuthenticated
      * @param string|null ...$guards
      * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
      */
-    public function handle(Request $request, Closure $next, ...$guards)
+    public function handle(Request $request, Closure $next, ...$guards): Response|RedirectResponse
     {
-        $guards = empty($guards) ? [null] : $guards;
-
-        foreach ($guards as $guard) {
-            if (Auth::guard($guard)->check()) {
-                return redirect(RouteServiceProvider::HOME);
-            }
+        if (!$request->expectsJson() && Auth::guard('sanctum')->check()) {
+            return redirect(RouteServiceProvider::HOME);
         }
 
         return $next($request);
