4694767fb8e8ba367f2acabc28b1fd4729d0c45a
Fabricio Zeferino <37294999+fabriciozeferino@users.noreply.github.com>
2022-03-13 08:19:56 +0000

    use alias @ to js root folder and separate config from webpack so the idea can pick that up

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,11 +1,17 @@
 const mix = require('laravel-mix')
 
+// Amend config in separated file for idea compatibility
+const config = require('./webpack.config.js')
+
+mix.webpackConfig(config)
+
 mix.browserSync({
   proxy: process.env.APP_URL,
   notify: true,
 })
 
 mix.js('resources/js/app.js', 'public/js').vue()
+
 mix.postCss('resources/css/app.css', 'public/css', [
   require('tailwindcss'),
 ])
